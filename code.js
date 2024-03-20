3db8ef81d8ed2cab79b51bfb3ed8d90897075238
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-03-19 21:29:29 -0600

    code.js

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
 function fib(n) {
-    if (n <= 1) {
-        return n; }
-    return fib(n-1) + fib(n-2); }
+    let sequence = [];
+    for (let i = 0; i <= n; i++) {
+        calculateFib(i);
+    return sequence; }
+
+    function calculateFib(index) {
+        if (index <= 1) {
+            return index; }
+        else {
+            if !(sequence[index]) {
+                sequence[index] = calculateFib(n-1) + calculateFib(n-2); }
+            return sequence[index]; }
