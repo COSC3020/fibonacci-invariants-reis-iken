9471e24b8ea1a867574815e96f555bf350593ec5
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-03-19 21:22:34 -0600

    code.js

--- code.js
+++ code.js
@@ -1,10 +1,4 @@
 function fib(n) {
     if (n <= 1) {
         return n; }
-    let previous = 0;
-    let current = 1;
-    for (let i = 2; i <= n; i++) {
-        let result = previous + current;
-        previous = current;
-        current = next; }
-    return current; }
+    return fib(n-1) + fib(n-2); }
