98bb2c3b8edcb1255f10bd21aaa0f0c44db5a146
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-03-19 22:15:22 -0600

    code.js

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 function fib(n) {
     var fib_solns = [];
     
-    function fibonacciCalculate(n) {
-        if (n === 0) return 0;
-        if (n === 1) return 1;
-        if (fib_solns[n] !== undefined) {
-            return fib_solns[n];
+    function fibonacciCalculate(i) {
+        if (i === 0) return 0;
+        if (i === 1) return 1;
+        if (fib_solns[i] !== undefined) {
+            return fib_solns[i];
         }
-        fib_solns[n] = fibonacci(n - 1) + fibonacci(n - 2);
-        return fib_solns[n];
+        fib_solns[i] = fibonacciCalculate(i-1) + fibonacciCalculate(i-2);
+        return fib_solns[i];
     }
     for (var i = 0; i <= n; i++) {
-        fibonacci(i);
+        fibonacciCalculate(i);
     }
     return fib_solns;
 }
