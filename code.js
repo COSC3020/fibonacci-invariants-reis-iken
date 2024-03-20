8f8399695768ae89b4c7f666b7ea3aa89fb498a7
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-03-19 21:02:44 -0600

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
+int result = 0;
 function fib(n) {
-    return n;
-}
+    if (n == 0) {
+        return result; }
+    else if (n == 1) {
+        result += 1; }
+    else {
+        result = fib(n-1) + fib(n-2); }
+    return result; }
